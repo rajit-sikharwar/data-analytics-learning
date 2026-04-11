7207446f854ce14c03dce8d4524ec410bb99341c
Rajit Sikharwar <140676816+rajit-sikharwar@users.noreply.github.com>
2026-04-12 02:28:46 +0530

    Add: Array diagonal element filled with 1's by eye() function

--- NumPy/specialNumpyArray2.py
+++ NumPy/specialNumpyArray2.py
@@ -6,6 +6,7 @@ print(arr1)
 print()
 arr2 = np.zeros((4,3))
 print(arr2)
+print()
 
 #Array filled with 1's
 arr_one = np.ones(5)
@@ -13,11 +14,24 @@ print(arr_one)
 print()
 arr_one1 = np.ones((5,4)) #5-rows, 4-columns matrix with all values 1
 print(arr_one1)
+print()
 
 #Create an Empty Array: empty array filled with elements that present in previous memory
 arr_empty = np.empty(5)
 print(arr_empty)
+print()
+arr_one1 = np.empty((5,4)) #5-rows, 4-columns matrix with all values 1
+print(arr_empty)
+print()
 
 #elements within a range
 arr_range = np.arange(5)
 print(arr_range) #[0 1 2 3 4]
+print()
+
+#Array diagonal element filled with 1's
+arr_diag = np.eye(3)
+print(arr_diag)
+print()
+arr_diag1 = np.eye(5,5)
+print(arr_diag1)
