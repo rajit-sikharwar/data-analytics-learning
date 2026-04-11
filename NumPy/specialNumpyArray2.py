5162365ece742ec3046d9cc02303f1ec8380303a
Rajit Sikharwar <140676816+rajit-sikharwar@users.noreply.github.com>
2026-04-12 02:05:18 +0530

    Add: #Array filled with 1's

--- NumPy/specialNumpyArray2.py
+++ NumPy/specialNumpyArray2.py
@@ -4,6 +4,12 @@ import numpy as np
 arr1 = np.zeros(4)
 print(arr1)
 print()
-
 arr2 = np.zeros((4,3))
 print(arr2)
+
+#Array filled with 1's
+arr_one = np.ones(5)
+print(arr_one)
+print()
+arr_one1 = np.ones((5,4)) #5-rows, 4-columns matrix with all values 1
+print(arr_one1)
