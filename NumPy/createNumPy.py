4296f977526181cef5c9f17695e1ccb185826087
Rajit Sikharwar <140676816+rajit-sikharwar@users.noreply.github.com>
2026-04-12 00:44:30 +0530

    Add: Use a tuple to create a NumPy array and create 0-D Arrays

--- NumPy/createNumPy.py
+++ NumPy/createNumPy.py
@@ -4,3 +4,14 @@ print(arr)
 
 #type check
 print(type(arr))
+
+
+#Use a tuple to create a NumPy array:
+arr = np.array((1, 2, 3, 4, 5))
+print(arr)
+
+
+#---------------Dimensions in Arrays----------
+#1. 0-D Array
+arr = np.array(100)
+print("\n0-D Array: \n", arr)
