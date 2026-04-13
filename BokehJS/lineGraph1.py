aa37de94f18590f1b71e8b5efc59fedb69f71cfe
Rajit Sikharwar <140676816+rajit-sikharwar@users.noreply.github.com>
2026-04-13 10:52:21 +0530

    Add: Legend Implementation

--- BokehJS/lineGraph1.py
+++ BokehJS/lineGraph1.py
@@ -10,3 +10,11 @@ y = [5, 4, 3, 2, 1]
 
 graph.line(x, y) 
 show(graph)
+
+#2. Legend Implementation
+# plotting the 1st line graph
+graph.line(x, x, legend_label="Line 1", line_color="green")
+
+# plotting the 2nd line graph with a different color
+graph.line(y, x, legend_label="Line 2", line_color="blue")
+show(graph)
