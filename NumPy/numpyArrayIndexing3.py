8249db977aa8aac343bad893ff0dd18c3540e859
Rajit Sikharwar <140676816+rajit-sikharwar@users.noreply.github.com>
2026-04-15 00:59:36 +0530

    Add: Accessing 3D arrays

--- NumPy/numpyArrayIndexing3.py
+++ NumPy/numpyArrayIndexing3.py
@@ -6,3 +6,7 @@ print(arr[2] + arr[3]) #sum of 3rd and 4th element
 #Accessing 2D arrays
 arr = np.array([[1,2,3,4,5], [6,7,8,9,10]])
 print('2nd element on 1st row: ', arr[0, 1])
+
+#Accessing 3D arrays
+arr = np.array([[[1, 2, 3],[4, 5, 6]],[[7, 8, 9],[10, 11, 12]]])
+print(arr[0, 1, 2])
