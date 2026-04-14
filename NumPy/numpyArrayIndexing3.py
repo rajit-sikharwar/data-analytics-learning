34f15d06d031c7701b513f933e668be210cbaa03
Rajit Sikharwar <140676816+rajit-sikharwar@users.noreply.github.com>
2026-04-15 00:37:52 +0530

    Add: Access 2D array

--- NumPy/numpyArrayIndexing3.py
+++ NumPy/numpyArrayIndexing3.py
@@ -2,3 +2,7 @@ import numpy as np
 arr = np.array([1, 2, 3, 4])
 print(arr[0])
 print(arr[2] + arr[3]) #sum of 3rd and 4th element
+
+#Accessing 2D arrays
+arr = np.array([[1,2,3,4,5], [6,7,8,9,10]])
+print('2nd element on 1st row: ', arr[0, 1])
