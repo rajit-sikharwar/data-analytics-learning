46e4b15639328b7e663216008688a3a42b0662cc
Rajit Sikharwar <140676816+rajit-sikharwar@users.noreply.github.com>
2026-04-15 01:10:57 +0530

    comment and semicolon

--- Python/Hello.py
+++ Python/Hello.py
@@ -2,8 +2,15 @@ print("Hello Rajit Sikharwar")
 
 #Python Indentation: the spaces at the beginning of a code line.
 if 10 > 5:
-  print("Five is greater than two!")
+  print("Ten is greater than two!")
 
 #Python Variables: A variable is a container for storing data values.
 x = 10
 y = "Hello, Rajit"
+
+#Commenting in Python: Comments can be used to explain code, and to make it more readable.
+#This is a comment.
+print("Hello, My name is Rajit Sikharwar")
+
+#semicolon can be used to separate multiple statements on the same line.
+print("Hello"); print("How are you?"); print("Bye bye!") 
