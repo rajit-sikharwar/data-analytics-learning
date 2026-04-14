8fb3b410983e96edf5e4796c2ad4051dcc38bcd8
Rajit Sikharwar <140676816+rajit-sikharwar@users.noreply.github.com>
2026-04-15 01:02:57 +0530

    variables

--- Python/Hello.py
+++ Python/Hello.py
@@ -3,3 +3,7 @@ print("Hello Rajit Sikharwar")
 #Python Indentation: the spaces at the beginning of a code line.
 if 10 > 5:
   print("Five is greater than two!")
+
+#Python Variables: A variable is a container for storing data values.
+x = 10
+y = "Hello, Rajit"
