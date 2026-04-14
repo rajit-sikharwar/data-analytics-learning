c7435012be99f0e55ef566f1dc9c717e60f9beb5
Rajit Sikharwar <140676816+rajit-sikharwar@users.noreply.github.com>
2026-04-14 23:18:55 +0530

    Clear the readability of comments

--- NumPy/specialNumpyArray2.py
+++ NumPy/specialNumpyArray2.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-#Array filled with 0's
+#-------------------Array filled with 0's
 arr1 = np.zeros(4)
 print(arr1)
 print()
@@ -8,7 +8,7 @@ arr2 = np.zeros((4,3))
 print(arr2)
 print()
 
-#Array filled with 1's
+#--------------------Array filled with 1's
 arr_one = np.ones(5)
 print(arr_one)
 print()
@@ -16,7 +16,7 @@ arr_one1 = np.ones((5,4)) #5-rows, 4-columns matrix with all values 1
 print(arr_one1)
 print()
 
-#Create an Empty Array: empty array filled with elements that present in previous memory
+#--------------------Create an Empty Array: empty array filled with elements that present in previous memory
 arr_empty = np.empty(5)
 print(arr_empty)
 print()
@@ -24,12 +24,12 @@ arr_one1 = np.empty((5,4)) #5-rows, 4-columns matrix with all values 1
 print(arr_empty)
 print()
 
-#elements within a range
+#-----------------------elements within a range
 arr_range = np.arange(5)
 print(arr_range) #[0 1 2 3 4]
 print()
 
-#Array diagonal element filled with 1's
+#--------------------Array diagonal element filled with 1's
 arr_diag = np.eye(3)
 print(arr_diag)
 print()
