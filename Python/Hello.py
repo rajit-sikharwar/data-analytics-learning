684b24fb8e6b276178c22ce1a100011e02290167
Rajit Sikharwar <140676816+rajit-sikharwar@users.noreply.github.com>
2026-04-15 01:23:21 +0530

    Add: Casting

--- Python/Hello.py
+++ Python/Hello.py
@@ -13,4 +13,9 @@ y = "Hello, Rajit"
 print("Hello, My name is Rajit Sikharwar")
 
 #semicolon can be used to separate multiple statements on the same line.
-print("Hello"); print("How are you?"); print("Bye bye!") 
+print("Hello"); print("How are you?"); print("Bye bye!")
+
+#Casting in Python: If you want to specify the data type of a variable, this can be done with casting.
+x = str(10)    # x will be '3'
+y = int(10)    # y will be 3
+z = float(10)  # z will be 3.0
