cacc030454368a68f43e48aa1f090c59f9904e01
Rajit Sikharwar <140676816+rajit-sikharwar@users.noreply.github.com>
2026-04-16 23:57:22 +0530

    add one more example

--- NumPy/numpyArraySlicing4.py
+++ NumPy/numpyArraySlicing4.py
@@ -1,3 +1,9 @@
 import numpy as np
 arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-print(arr[1:5]) #Slice elements from index 1 to index 5 from the array
+print(arr[1:5]) #Slice elements from index 1 to index 5 from the
+
+
+
+#Slice elements from index 5 to the end of the array:
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+print(arr[5:])
