b605e2c0f96398057d8d9c2bdee535d8e85d46d0
Rajit Sikharwar <140676816+rajit-sikharwar@users.noreply.github.com>
2026-04-16 23:35:19 +0530

    Add: Negative indexing

--- NumPy/numpyArrayIndexing3.py
+++ NumPy/numpyArrayIndexing3.py
@@ -10,3 +10,7 @@ print('2nd element on 1st row: ', arr[0, 1])
 #Accessing 3D arrays
 arr = np.array([[[1, 2, 3],[4, 5, 6]],[[7, 8, 9],[10, 11, 12]]])
 print(arr[0, 1, 2])
+
+#Negative Indexing
+arr = np.array([[1,2,3,4,5], [6,7,8,9,10]])
+print('Last element from 2nd dim: ', arr[1, -1])
