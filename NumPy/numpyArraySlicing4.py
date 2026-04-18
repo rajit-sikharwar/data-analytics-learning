54ad598eed4761031f31166969c72331fe9bcdb0
Rajit Sikharwar <140676816+rajit-sikharwar@users.noreply.github.com>
2026-04-18 23:41:58 +0530

    Add: Slice elements from the beginning to index[not including]

--- NumPy/numpyArraySlicing4.py
+++ NumPy/numpyArraySlicing4.py
@@ -2,8 +2,10 @@ import numpy as np
 arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 print(arr[1:5]) #Slice elements from index 1 to index 5 from the
 
-
-
 #Slice elements from index 5 to the end of the array:
 arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 print(arr[5:])
+
+#Slice elements from the beginning to index 5 (not included):
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+print(arr[:5])
