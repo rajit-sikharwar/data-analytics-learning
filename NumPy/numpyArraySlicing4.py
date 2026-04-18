ea9350d47ff0b3b285634a348f054b91a1d0cad3
Rajit Sikharwar <140676816+rajit-sikharwar@users.noreply.github.com>
2026-04-18 23:43:52 +0530

    Add: Negative Slicing

--- NumPy/numpyArraySlicing4.py
+++ NumPy/numpyArraySlicing4.py
@@ -9,3 +9,7 @@ print(arr[5:])
 #Slice elements from the beginning to index 5 (not included):
 arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 print(arr[:5])
+
+#Negative Slicing: Use the minus operator to refer to an index from the end:
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+print(arr[-3:-1])
