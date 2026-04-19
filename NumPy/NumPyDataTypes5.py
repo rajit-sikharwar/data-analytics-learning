c2b01b2339be7c56495bad0a1883f6c755c67e0c
Rajit Sikharwar <140676816+rajit-sikharwar@users.noreply.github.com>
2026-04-19 23:50:54 +0530

    bool type

--- NumPy/NumPyDataTypes5.py
+++ NumPy/NumPyDataTypes5.py
@@ -1,4 +1,10 @@
 import numpy as np
+#integer
 arr = np.array([1, 2, 3, 4, 5], dtype='i') #integer
+print(arr)
+print(arr.dtype)
+
+#bool
+arr = np.array([True, False, True]) 
 print(arr)
 print(arr.dtype)
