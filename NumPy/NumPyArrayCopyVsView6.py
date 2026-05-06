3667c22d693c2c2199f70e745d7919abc5a71b17
Rajit Sikharwar <140676816+rajit-sikharwar@users.noreply.github.com>
2026-05-07 00:09:47 +0530

    Add: Include additional examples for array views and copies, and print base attribute values

--- NumPy/NumPyArrayCopyVsView6.py
+++ NumPy/NumPyArrayCopyVsView6.py
@@ -20,3 +20,10 @@ x = arr.view()
 x[0] = 31
 print(arr)
 print(x)
+
+# Print the value of the base attribute to check if an array owns it's data or not:
+arr = np.array([1, 2, 3, 4, 5])
+x = arr.copy()
+y = arr.view()
+print(x.base)
+print(y.base)
