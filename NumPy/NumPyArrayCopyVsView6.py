a4a2f5d58900a6369f40acd096d02c186609ffab
Rajit Sikharwar <140676816+rajit-sikharwar@users.noreply.github.com>
2026-05-07 00:05:29 +0530

    Add: Display modified view after changing the original array

--- NumPy/NumPyArrayCopyVsView6.py
+++ NumPy/NumPyArrayCopyVsView6.py
@@ -13,3 +13,10 @@ x = arr.view()
 arr[0] = 42
 print(arr)
 print(x)
+
+# Make a view, change the view, and display both arrays:
+arr = np.array([1, 2, 3, 4, 5])
+x = arr.view()
+x[0] = 31
+print(arr)
+print(x)
