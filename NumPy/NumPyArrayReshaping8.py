beb7d8ec027e3467eac3d9e7425601a0c97d5933
Rajit Sikharwar <140676816+rajit-sikharwar@users.noreply.github.com>
2026-05-09 00:07:29 +0530

    Add: Include check for array view or copy in reshaping examples

--- NumPy/NumPyArrayReshaping8.py
+++ NumPy/NumPyArrayReshaping8.py
@@ -10,3 +10,8 @@ print(newarr)
 arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
 newarr = arr.reshape(2, 3, 2)
 print(newarr)
+
+#Check if the returned array is a copy or a view:
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+print(arr.reshape(2, 4).base) #returns the original array, so it is a view.
+
