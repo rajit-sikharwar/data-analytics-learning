255df2cfb57dffcc405270c61d87b7fbe4a7a86c
Rajit Sikharwar <140676816+rajit-sikharwar@users.noreply.github.com>
2026-08-02 20:48:34 +0530

    Add: Update marker in plot example and expand marker options documentation

--- Matplotlib/MatplotlibMarkers.py
+++ Matplotlib/MatplotlibMarkers.py
@@ -1,4 +1,33 @@
 import matplotlib.pyplot as plt
 x = [3, 8, 1, 10]
-plt.plot(x, marker = 'o')
+plt.plot(x, marker = '+')
 plt.show()
+
+'''
+
+'o'	Circle	
+'*'	Star	
+'.'	Point	
+','	Pixel	
+'x'	X	
+'X'	X (filled)	
+'+'	Plus	
+'P'	Plus (filled)	
+'s'	Square	
+'D'	Diamond	
+'d'	Diamond (thin)	
+'p'	Pentagon	
+'H'	Hexagon	
+'h'	Hexagon	
+'v'	Triangle Down	
+'^'	Triangle Up	
+'<'	Triangle Left	
+'>'	Triangle Right	
+'1'	Tri Down	
+'2'	Tri Up	
+'3'	Tri Left	
+'4'	Tri Right	
+'|'	Vline	
+'_'	Hline
+
+'''
