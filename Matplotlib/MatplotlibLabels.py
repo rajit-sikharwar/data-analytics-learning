635eda32e481be28063daee299f15616b66d4beb
Rajit Sikharwar <140676816+rajit-sikharwar@users.noreply.github.com>
2026-08-04 22:21:01 +0530

    Fix: Enhance title and xlabel formatting with custom font styles

--- Matplotlib/MatplotlibLabels.py
+++ Matplotlib/MatplotlibLabels.py
@@ -9,8 +9,19 @@ plt.plot(
     color="b",
     label="Student Progress",
 )
-plt.title("Impact of Study Hours on Test Scores", fontsize=14, fontweight="bold")
-plt.xlabel("Hours Spent Studying", fontsize=12)
+
+font1 = {'family':'serif','color':'blue','size':15}
+font2 = {'family':'serif','color':'darkred','size':10}
+
+
+plt.title("Impact of Study Hours on Test Scores",
+          fontsize=14,
+          fontweight="bold",
+          fontdict = font1, 
+          loc = 'left'
+          )
+
+plt.xlabel("Hours Spent Studying", fontsize=12, fontdict = font2)
 plt.ylabel("Test Score (%)", fontsize=12)
 plt.legend(loc="upper left")
 plt.grid(True, linestyle="--", alpha=0.6)
