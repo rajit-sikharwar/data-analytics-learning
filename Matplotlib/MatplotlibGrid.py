170f8c4f84b59e960cd3f7dc872dbfc52e85eb52
Rajit Sikharwar <140676816+rajit-sikharwar@users.noreply.github.com>
2026-08-04 22:28:42 +0530

    Add: Create initial plot to visualize the impact of study hours on test scores(Grid Implement)

---
+++ Matplotlib/MatplotlibGrid.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+study_hours = [3, 6, 9, 10, 12]
+test_scores = [60, 75, 80, 90, 98]
+plt.plot(
+    study_hours,
+    test_scores,
+    marker="o",
+    linestyle="-",
+    color="b",
+    label="Student Progress",
+)
+
+plt.title("Impact of Study Hours on Test Scores",
+          fontsize=14,
+          fontweight="bold",
+          loc = 'left'
+          )
+
+plt.xlabel("Hours Spent Studying", fontsize=12)
+plt.ylabel("Test Score (%)", fontsize=12)
+plt.legend(loc="upper left")
+plt.grid(True, linestyle="--", alpha=0.6) #Grid lines added to the plot for better readability
+plt.show()
